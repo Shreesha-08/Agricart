c601afa1b4789ba511f33f580d686f318ad955a1
Shreesha <Shreesha.082@gmail.com>
2021-12-12 17:45:47 +0530

    webapp project

--- Agri webapp/app.py
+++ Agri webapp/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
-
 app = Flask(__name__,template_folder='template')
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def homePage():
     return render_template("index.html")
 
 if __name__ == "__main__":
