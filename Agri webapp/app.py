4bc77622d9d01a61912029c70519b86612aa0279
Shreesha <Shreesha.082@gmail.com>
2022-01-09 13:26:47 +0530

    ignore

--- Agri webapp/app.py
+++ Agri webapp/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__,template_folder='template')
 
 @app.route('/')
 def homePage():
-    name = "Shreesha"
     return render_template("index.html")
+
+@app.route('/farmersHomePage', methods=["POST"])
+def farmersPage():
+    if request.method == "POST":
+        if request.form["email"] == "shreesha":
+            return "Hello"
+        else:
+            return render_template("index.html")
+
+@app.route('/retailersHomePage', methods=["POST"])
+def retailersPage():
+    if request.method == "POST":
+        if request.form["email"] == "shreesha":
+            return "Hello"
+        else:
+            return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
