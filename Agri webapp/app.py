8a5a41f1d4d78874dea0edfebf0be61a2d4bf787
Shreesha <Shreesha.082@gmail.com>
2022-01-11 19:53:09 +0530

    ignore

--- Agri webapp/app.py
+++ Agri webapp/app.py
@@ -1,7 +1,9 @@
 from typing_extensions import ParamSpecKwargs
+from werkzeug.security import generate_password_hash, check_password_hash
 from db import *
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask,flash, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__,template_folder='template')
 
@@ -13,6 +15,17 @@ app.secret_key = 'many random bytes'
 
 mysql = MySQL(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM farmers where f_id = {user_id}" )
+    datas = cur.fetchall()
+    cur.close()
+    return datas
+
 with app.app_context():
     cur = mysql.connection.cursor()
     cur.execute("SELECT  * FROM farmers")
@@ -20,18 +33,27 @@ with app.app_context():
     print(data)
     cur.close()
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def homePage():
     return render_template("index.html")
 
-@app.route('/farmersHomePage', methods=["POST"])
+class User(UserMixin):
+    id= ""
+    name = ""
+    password = ""
+
+@app.route('/farmersHomePage', methods=["POST", 'GET'])
 def farmersPage():
     if request.method == "POST":
-        if request.form["username"] == "shreesha":
-            newLogin = {request.form["username"]: request.form["password"]}
-            return "Hello"
-        else:
-            return render_template("index.html")
+        flag=0
+        user = User()
+        user.name = request.form["username"]
+        user.password = request.form["password"]
+        flag = dbAct.check_login_farmers(user)
+        if flag == 1:
+            login_user(user)
+            return render_template("registerF.html")
+    return render_template("index.html")
 
 class InsertForRegistration:
     def __init__(self,name,password,phno):
@@ -47,22 +69,67 @@ def fRegPage():
 def rRegPage():
     return render_template("registerR.html")
 
-@app.route('/registerFarmers', methods=["POST"])
+@app.route('/registerFarmers', methods=["POST", 'GET'])
 def farmerRegister():
     if request.method == "POST":
-        insertFarmers = InsertForRegistration(request.form["fname"], request.form["psw"], request.form["phno"])
+        pw = request.form["psw"]
+        hashedPassword = generate_password_hash(
+            pw,
+            method='pbkdf2:sha256',
+            salt_length=3
+        )
+        print(hashedPassword)
+        insertFarmers = InsertForRegistration(request.form["fname"], hashedPassword, request.form["phno"])
         dbAct.insert_to_farmers(insertFarmers)
 
-@app.route('/registerRetailers', methods=["POST"])
+@app.route('/registerRetailers', methods=["POST", 'GET'])
 def retailerRegister():
     if request.method == "POST":
-        insertRetailer = InsertForRegistration(request.form["rname"], request.form["psw"], request.form["phno"])
+        if dbAct.check_for_user(request.form["rname"]):
+            flash("Username already exists!")
+            return render_template("registerR.html")
+        hashedPassword = generate_password_hash(
+            request.form.get('psw'),
+            method='pbkdf2:sha256',
+            salt_length=3
+        )
+        insertRetailer = InsertForRegistration(request.form["rname"], hashedPassword, request.form["phno"])
         dbAct.insert_to_retailers(insertRetailer)
+    return render_template("registerR.html")
 
-@app.route('/retailersHomePage', methods=["POST"])
+@app.route('/retailersHomePage', methods=["POST", 'GET'])
 def retailersPage():
     if request.method == "POST":
-        return "Hello Buyer"
+        user = User()
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM customers WHERE c_name=%s",[request.form["username"]])
+        usr = cur.fetchall()
+        user.id = int(usr[0][0])
+        user.name = usr[0][1]
+        user.password = usr[0][2]
+        print(user)
+        cur.close()
+        if not user.name:
+            flash("Username does not exist, please try again.")
+            return render_template("index.html")
+        elif not check_password_hash(user.password, request.form["password"]):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            # flag=0
+            # user = User()
+            # user.name = request.form["username"]
+            # user.password = request.form["password"]
+            # flag = dbAct.check_login_retailers(user)
+            # if flag == 1:
+            login_user(user)
+            return render_template("retailerLogin.html")
+    return render_template("index.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Agri webapp/db.py
+++ Agri webapp/db.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__,template_folder='template')
 app.config['MYSQL_HOST'] = 'localhost'
@@ -8,6 +9,14 @@ app.config['MYSQL_PASSWORD'] = '@0802root'
 app.config['MYSQL_DB'] = 'agri'
 
 mysql = MySQL(app)
+
+with app.app_context():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM farmers")
+    fdata = cur.fetchall()
+    cur.execute("SELECT  * FROM customers")
+    rdata = cur.fetchall()
+    cur.close()
 
 class DatabaseActivities:
     def insert_to_farmers(self,obj):
@@ -23,5 +32,44 @@ class DatabaseActivities:
             cur.execute("INSERT INTO customers (c_name, c_pwd, ph_no) VALUES (%s, %s, %s)", (obj.name, obj.password, obj.phno))
             mysql.connection.commit()
             return render_template("index")
+
+    def check_login_farmers(self,details):
+        flag=0
+        j=0
+        while j < len(fdata):
+            if check_password_hash(fdata[j][2], details.password) and (details.name==fdata[j][1]):
+                details.id = fdata[j][0]
+                flag=1
+            j+=1
+        return flag
+    
+    def check_login_retailers(self,details):
+        flag=0
+        j=0
+        print(rdata)
+        while j < len(rdata):
+            if check_password_hash(rdata[j][2], details.password) and (details.name==rdata[j][1]):
+                details.id = rdata[j][0]
+                flag=1
+            j+=1
+        return flag
+    
+    def get_pwd(self, usr):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT  * FROM customers WHERE c_name=%s",[usr])
+        pwd = cur.fetchall()
+        cur.close()
+        print(pwd)
+        return pwd[0][2]
+
+    def check_for_user(self,usr):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT  * FROM customers WHERE c_name=%s",[usr])
+        flag = cur.fetchall()
+        cur.close()
+        if flag:
+            return True
+        else:
+            return False
 
 dbAct = DatabaseActivities()
