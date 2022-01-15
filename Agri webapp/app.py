5d4d2fb2fd710378d160914e7fdfedfe1baba14e
Shreesha <Shreesha.082@gmail.com>
2022-01-15 20:24:25 +0530

    update and delete crops completed

--- Agri webapp/app.py
+++ Agri webapp/app.py
@@ -18,7 +18,6 @@ with app.app_context():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM farmers")
     data = cur.fetchall()
-    print(data)
     cur.close()
 
 @app.route('/')
@@ -40,23 +39,21 @@ def farmersPage():
         flag = dbAct.check_login_farmers(user)
         if flag == 1:
             session["user_id"]=user.id
-            return render_template("farmerHome.html")
+            crops = ()
+            crops = dbAct.getAllCrops(session["user_id"])
+            return render_template("farmerHome.html", crops=crops)
         else:
             return render_template("index.html", status1 = flag)
+    crops = ()
+    crops = dbAct.getAllCrops(session["user_id"])
+    print(crops)
+    return render_template("farmerHome.html", crops=crops)
 
 class InsertForRegistration:
     def __init__(self,name,password,phno):
         self.name = name
         self.password = password
         self.phno = phno
-
-# @app.route('/registerF.html')
-# def fRegPage():
-#     return render_template("registerF.html")
-
-# @app.route('/registerR.html')
-# def rRegPage():
-#     return render_template("registerR.html")
 
 @app.route('/registerFarmers', methods=["POST", 'GET'])
 def farmerRegister():
@@ -105,9 +102,31 @@ def retailersPage():
         else:
             return render_template("index.html", status2 = flag)
 
+class Crop:
+    cName = ""
+    quantity = 0
+    price = 0
+    imgLocation = ""
+
+@app.route("/updatestock", methods=["GET","POST"])
+def updateStock():
+    if request.method == "POST":
+        crop = Crop()
+        crop.cName = request.form["crop"]
+        crop.quantity = request.form["quantity"]
+        crop.price = request.form["price"]
+        dbAct.addCrop(crop,session["user_id"])
+        return redirect(url_for('farmersPage'))
+    return render_template("updateStock.html")
+
+@app.route("/deletecrop/<name>")
+def deleteCrop(name):
+    dbAct.deleteCrop(name)
+    flash(f"{name} is removed successfully.")
+    return redirect(url_for('farmersPage'))
+
 @app.route('/logout')
 def logout():
-    # logout_user()
     session["user_id"] = None
     return redirect(url_for('homePage'))
 
--- Agri webapp/db.py
+++ Agri webapp/db.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
 
 app = Flask(__name__,template_folder='template')
 app.config['MYSQL_HOST'] = 'localhost'
@@ -16,6 +17,8 @@ with app.app_context():
     fdata = cur.fetchall()
     cur.execute("SELECT  * FROM customers")
     rdata = cur.fetchall()
+    cur.execute("SELECT  * FROM stock")
+    sdata = cur.fetchall()
     cur.close()
 
 class DatabaseActivities:
@@ -24,12 +27,14 @@ class DatabaseActivities:
             cur = mysql.connection.cursor()
             cur.execute("INSERT INTO farmers (f_name, f_pwd, ph_no) VALUES (%s, %s, %s)", (obj.name, obj.password, obj.phno))
             mysql.connection.commit()
+            cur.close()
     
     def insert_to_retailers(self,obj):
         with app.app_context():
             cur = mysql.connection.cursor()
             cur.execute("INSERT INTO customers (c_name, c_pwd, ph_no) VALUES (%s, %s, %s)", (obj.name, obj.password, obj.phno))
             mysql.connection.commit()
+            cur.close()
 
     def check_login_farmers(self,details):
         flag=0
@@ -71,12 +76,52 @@ class DatabaseActivities:
 
     def check_for_userC(self,usr):
         cur = mysql.connection.cursor()
-        cur.execute("SELECT  * FROM customers WHERE c_name=%s",[usr])
+        cur.execute("SELECT * FROM customers WHERE c_name=%s",[usr])
         flag = cur.fetchall()
         cur.close()
         if flag:
             return True
         else:
             return False
+
+    def addCrop(self, crop, fid):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM stock WHERE f_id=%s AND crop=%s",(fid, crop.cName))
+        exists = cur.fetchall()
+        crop.imgLocation = exists[0][4]
+        print(exists[0][4])
+        if exists:
+            updatedQuantity = int(crop.quantity) + int(exists[0][2])
+            updatedPrice = int(crop.price) + int(exists[0][3])
+            cur.execute("UPDATE stock SET quantity=%s, price=%s WHERE f_id=%s AND crop=%s", (updatedQuantity, updatedPrice, fid, crop.cName))
+            mysql.connection.commit()
+        else:
+            cur.execute("INSERT INTO stock (crop, f_id, quantity, price) VALUES (%s, %s, %s, %s)", (crop.cName, fid, int(crop.quantity), int(crop.price)))
+            mysql.connection.commit()
+        cur.close()
+        
+    def getAllCrops(self, fid):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM stock WHERE f_id=%s", [fid])
+        allCrops = cur.fetchall()
+        cur.close()
+        return allCrops
+
+    # def getCropsList(self, crops):
+    #     heroRegex = re.compile(r'rice|Rice|dal|Dal')
+    #     cropsList = []
+    #     print(crops)
+    #     for i in range(len(crops)):
+    #         mo1 = heroRegex.findall(crops[i][0])
+    #         print(mo1)
+    #         if mo1:
+    #             cropsList += mo1
+    #     return cropsList
+
+    def deleteCrop(self,cName):
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM stock WHERE crop=%s", [cName])
+        mysql.connection.commit()
+        cur.close()
 
 dbAct = DatabaseActivities()
